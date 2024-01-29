f9ac7afd0a7a6203e104f9fa5ff8f63a3a42188c
calanfive <camille.lelan@gmail.com>
2024-01-29 16:16:15 +0100

    if params into body : notion of priority

--- pages/api/hello/[name].ts
+++ pages/api/hello/[name].ts
@@ -8,6 +8,6 @@ type Data = {
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
-) {
-  res.status(200).json({ message: "bonjour" + req.query.name});
-}
+  ) {    
+    res.status(200).json({ message: "bonjour" + (req.body.trueName || req.query.name)});
+  }
