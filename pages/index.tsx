ee4ced4b9db4d5b1aa4afe6ac5d1a7607e19c7c8
calanfive <camille.lelan@gmail.com>
2024-01-29 12:04:41 +0100

    SSR route API GET

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import Head from "next/head";
 import { useEffect, useState } from "react";
 
-export const getServerSideProps = () => {
-  return { props: { dateStringSSR: new Date().toISOString() } }
+export const getServerSideProps = async () => {
+  const res = await fetch(`http://localhost:3002/api/date`)
+  const data = await res.json()
+  return { props: { dateStringSSR: data.date } }
 }
 
 // export const getStaticProps = () => {
